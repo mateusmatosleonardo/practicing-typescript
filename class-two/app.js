4f133461a1ca8f906f791be28f0b937eb824dd12
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-03 17:51:16 -0300

    adding any

--- class-two/app.js
+++ class-two/app.js
@@ -1,4 +1,10 @@
 /* Arrays */
+/* Array type string */
 var arr;
 arr = ['Mateus', 'Lucas', 'Vinicius'];
 console.log(arr[0]);
+/* =============================================== */
+/* any */
+/* any interprets any type of variable */
+var exp = '10';
+console.log(exp);
--- class-two/app.ts
+++ class-two/app.ts
@@ -6,3 +6,14 @@ var arr: string [];
 arr = ['Mateus', 'Lucas', 'Vinicius'];
 
 console.log(arr[0]);
+
+/* =============================================== */
+
+/* any */
+
+/* any interprets any type of variable */
+
+var exp: any = '10';
+
+console.log(exp);
+                                        
