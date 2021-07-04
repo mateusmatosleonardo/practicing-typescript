952dcb1ef16f81484c3f6f7c4d82e97823f67e73
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-04 17:50:38 -0300

    creating method

--- class-four/app.ts
+++ class-four/app.ts
@@ -7,11 +7,23 @@ class Person{
     constructor(nome:string){
         this.nome = nome;
     }
+
+    /* Method */
+    printName(){
+        return this.nome;
+    }
 }
 
+var personOne = new Person("Mateus");
+
+console.log(personOne.printName());
+
+
+/*
 var personOne = new Person("Mateus");
 
 var personTwo = new Person("Leonardo");
 
 console.log(personOne.nome);
 console.log(personTwo.nome);
+*/
