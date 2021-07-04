820e74c25ce1483fe3e3c9e1b04eb70fa6b4bc97
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-04 18:05:04 -0300

    transpilando

--- class-four/app.js
+++ class-four/app.js
@@ -1,4 +1,19 @@
 /* Orientado a objeto */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(nome) {
         this.nome = nome;
@@ -7,15 +22,18 @@ var Person = /** @class */ (function () {
     Person.prototype.printName = function () {
         return this.nome;
     };
-    Person.prototype.printIdade = function () {
-        return this.idade;
-    };
     return Person;
 }());
-var personOne = new Person("Mateus");
-var personOne = new Person('21');
-console.log(personOne.printName());
-console.log(personOne.printIdade());
+/* extends herda todos os métodos e parâmetros (atributos) */
+var user = /** @class */ (function (_super) {
+    __extends(user, _super);
+    function user(nome) {
+        return _super.call(this, nome) || this;
+    }
+    return user;
+}(Person));
+var userOne = new user("Mateus");
+console.log(userOne.printName());
 /*
 var personOne = new Person("Mateus");
 
