5005e5bae807bc52c63fd93f6ed2dca6633b39d0
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-04 17:52:09 -0300

    transpilando

--- class-four/app.js
+++ class-four/app.js
@@ -3,9 +3,19 @@ var Person = /** @class */ (function () {
     function Person(nome) {
         this.nome = nome;
     }
+    /* Method */
+    Person.prototype.printName = function () {
+        return this.nome;
+    };
     return Person;
 }());
 var personOne = new Person("Mateus");
+console.log(personOne.printName());
+/*
+var personOne = new Person("Mateus");
+
 var personTwo = new Person("Leonardo");
+
 console.log(personOne.nome);
 console.log(personTwo.nome);
+*/ 
